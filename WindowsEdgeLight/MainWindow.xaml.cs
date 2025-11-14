f0ddde3984d3a5cd38479f944ab40b5a66c7bdaf
Scott Hanselman <scott@hanselman.com>
2025-11-14 11:12:18 -0800

    Initial commit - version 0.1

---
+++ WindowsEdgeLight/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace WindowsEdgeLight;
+
+public partial class MainWindow : Window
+{
+    private bool isLightOn = true;
+    private double currentOpacity = 0.95;
+    private const double OpacityStep = 0.15;
+    private const double MinOpacity = 0.2;
+    private const double MaxOpacity = 1.0;
+
+    [DllImport("user32.dll")]
+    private static extern int GetSystemMetrics(int nIndex);
+    private const int SM_CXSCREEN = 0;
+    private const int SM_CYSCREEN = 1;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        SetupWindow();
+    }
+
+    private void SetupWindow()
+    {
+        var workingArea = SystemParameters.WorkArea;
+        var screenWidth = workingArea.Width;
+        var screenHeight = workingArea.Height;
+
+        this.Left = workingArea.Left;
+        this.Top = workingArea.Top;
+        this.Width = screenWidth;
+        this.Height = screenHeight;
+
+        EdgeLightBorder.Margin = new Thickness(20);
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        var hwnd = new WindowInteropHelper(this).Handle;
+        int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+        SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED);
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.L && 
+            (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control && 
+            (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift)
+        {
+            ToggleLight();
+        }
+        else if (e.Key == Key.Escape)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+
+    private void Toggle_Click(object sender, RoutedEventArgs e)
+    {
+        ToggleLight();
+    }
+
+    private void ToggleLight()
+    {
+        isLightOn = !isLightOn;
+        EdgeLightBorder.Visibility = isLightOn ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    private void BrightnessUp_Click(object sender, RoutedEventArgs e)
+    {
+        currentOpacity = Math.Min(MaxOpacity, currentOpacity + OpacityStep);
+        EdgeLightBorder.Opacity = currentOpacity;
+    }
+
+    private void BrightnessDown_Click(object sender, RoutedEventArgs e)
+    {
+        currentOpacity = Math.Max(MinOpacity, currentOpacity - OpacityStep);
+        EdgeLightBorder.Opacity = currentOpacity;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_TRANSPARENT = 0x00000020;
+    private const int WS_EX_LAYERED = 0x00080000;
+
+    [DllImport("user32.dll")]
+    private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+}
